a9edc16f31093a6ff17f0605aa08dc6cf34df317
Rakesh <byreddyrakesh41@gmail.com>
2022-10-26 08:41:08 +0530

    ADDED: Code for the Starting Position

---
+++ src/snakeladder/StartPosition.java
@@ -0,0 +1,8 @@
+package snakeladder;
+
+public class StartPosition {
+    public static void main(String[] args) {
+        int pos = 0;
+        System.out.println("Single Player at Start Position "+pos);
+    }
+}
